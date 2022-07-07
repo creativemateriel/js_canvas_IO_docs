85fd4b91447a36100dda000e701ddb9fc72ee297
UnacceptableBehaviour <fernandez.simon@gmail.com>
2022-07-07 16:03:29 +0100

    remove cert from PWA POC

--- docs/static/js_modules/app.js
+++ docs/static/js_modules/app.js
@@ -1,7 +1,7 @@
 // PWA Template 
 
 import * as navBarMod from './navbarMod.js';
-import * as pageHome from './module_page_home.js';
+//import * as pageHome from './module_page_home.js';
 //import * as pageTracker from './module_page_tracker.js';
 //import * as pageSnap from './module_page_snap.js';
 import * as pageWeighIn from './module_page_weigh_in.js';
@@ -20,7 +20,7 @@ var containers = navBarMod.getContainers();
 //var buttonInfo = pageTable.getButtonInfo(containers);
 //navBarMod.addNavbutton(buttonInfo);
 // order in which they appear on the navbar . .
-navBarMod.addNavbutton(pageHome.getButtonInfo(containers));
+//navBarMod.addNavbutton(pageHome.getButtonInfo(containers));
 //navBarMod.addNavbutton(pageTracker.getButtonInfo(containers));
 //navBarMod.addNavbutton(pageSnap.getButtonInfo(containers));
 navBarMod.addNavbutton(pageWeighIn.getButtonInfo(containers));
